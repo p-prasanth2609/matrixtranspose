9234ae5c3e98ac078de16c9a3d432edd5178a01d
satheeshkumar <psatheesh21120@gamil.com>
2023-07-19 09:21:47 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+r = int(input())
+c = int(input())
+A = []
+for i in range(r):
+    row = []
+    for j in range(c):
+        element = int(input())
+        row.append(element)
+    A.append(row)
+transpose = []
+for j in range(c):
+    transposed_row = []
+    for i in range(r):
+        transposed_row.append(A[i][j])
+    transpose.append(transposed_row)
+for row in transpose:
+    print(row)
